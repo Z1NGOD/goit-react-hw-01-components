1a8318a83360ab88a0283ad972786f82d1bf010a
Z1NG0D <borovskiyyaroslavv@gmail.com>
2023-07-30 21:42:22 +0200

    comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import Profile from "./Profile/Profile";
-import Statistics from "./Statistics/Statistics";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
-import user from "../json/user.json";
-import data from "../json/data.json";
-import friends from "../json/friends.json";
-import transactions from "../json/transactions.json";
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import user from '../json/user.json';
+import data from '../json/data.json';
+import friends from '../json/friends.json';
+import transactions from '../json/transactions.json';
 
-function App() {
+export const App = () => {
   return (
     <>
       <Profile
@@ -19,9 +19,7 @@ function App() {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
-      <TransactionHistory transactions={transactions}/>
+      <TransactionHistory transactions={transactions} />
     </>
   );
-}
-
-export default App;
+};
